439312cb87de7964bce826bf5ee2c273089578d2
jonghae <bdfire1234@snu.ac.kr>
2024-03-23 17:37:11 +0900

    add 1dim task like hopper

--- envs/register.py
+++ envs/register.py
@@ -10,10 +10,47 @@ def register_custom_envs():
         reward_threshold=6000,
     )
 
-
     register(
         id="Point-v1",
         entry_point='envs.mujoco.point_v1:PointEnv',
         max_episode_steps=200,
         reward_threshold=6000,
+    )
+    
+    register(
+        id="Hopper-v2",
+        entry_point="envs.mujoco:HopperEnv",
+        max_episode_steps=1000,
+        reward_threshold=3800.0,
+    )
+
+    register(
+	    id="Hopper-v3",
+	    entry_point="envs.mujoco.hopper_v3:HopperEnv",
+	    max_episode_steps=1000,
+	    reward_threshold=3800.0,
+	)
+	
+    register(
+        id="Walker2d-v2",
+        max_episode_steps=1000,
+        entry_point="envs.mujoco:Walker2dEnv",
+    )
+
+    register(
+        id="Walker2d-v3",
+        max_episode_steps=1000,
+        entry_point="envs.mujoco.walker2d_v3:Walker2dEnv",
+    )
+
+    register(
+        id="Humanoid-v2",
+        entry_point="envs.mujoco:HumanoidEnv",
+        max_episode_steps=1000,
+    )
+
+    register(
+        id="Humanoid-v3",
+        entry_point="envs.mujoco.humanoid_v3:HumanoidEnv",
+        max_episode_steps=1000,
     )
